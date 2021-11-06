afc0aecb1d8e4e0f3d34bc1e176e7da2c79d579c
Suguru Hirakawa <hi.suguru@gmail.com>
2021-11-07 04:38:06 +0900

    Add Synack

--- Synack Red Team Five CTF/crypto/crypto_leakeyd/script.py
+++ Synack Red Team Five CTF/crypto/crypto_leakeyd/script.py
@@ -1,13 +1,14 @@
 from Crypto.Util.number import getPrime, bytes_to_long
 from math import gcd
 
-flag = open("flag.txt").read().strip().encode()
+# flag = open("flag.txt").read().strip().encode()
+flag = b"fl"
 
-p = getPrime(1024)
-q = getPrime(1024)
+p = getPrime(8)
+q = getPrime(8)
 n = p * q
-e1 = 0x10001
-e2 = 0x13369
+e1 = 0x5
+e2 = 0x7
 
 
 assert gcd(p-1,e1) == 1 and gcd(q-1, e1) == 1 and gcd(p-1,e2) == 1 and gcd(q-1, e2) == 1
@@ -20,6 +21,8 @@ e = {e1}
 d = {d1}""")
 
 ct = pow(bytes_to_long(flag), e2, n)
-print(f"""Spy messages: 
+print(f"""Spy messages:
 e = {e2}
 ct = {ct}""")
+
+print(p, q)
